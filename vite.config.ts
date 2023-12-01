020f38a42e229d502c9460d85860edf59aead943
vloddot <t.mokhtar.vd@gmail.com>
2023-12-01 10:06:01 +0200

    fix: it's -solidjs :facepalm:

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
 	plugins: [solid()],
-	base: process.env.NODE_ENV == 'production' ? '/jolt' : '',
+	base: process.env.NODE_ENV == 'production' ? '/jolt-solidjs' : '',
 
 	css: {
 		modules: {
