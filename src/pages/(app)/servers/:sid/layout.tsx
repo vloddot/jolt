79d63efa340422964eff7d797840b8522e20a35a
vloddot <t.mokhtar.vd@gmail.com>
2023-12-01 22:00:55 +0200

    refactor: channel bar to component

--- src/pages/(app)/servers/:sid/layout.tsx
+++ src/pages/(app)/servers/:sid/layout.tsx
@@ -1,27 +1,17 @@
 import { SelectedServerIdContext } from '@lib/context/SelectedServerId';
 import { Outlet } from '@solidjs/router';
-import {
-	For,
-	Match,
-	Show,
-	Switch,
-	createMemo,
-	createSelector,
-	useContext
-} from 'solid-js';
+import { For, Match, Show, Switch, createMemo, createSelector, useContext } from 'solid-js';
 import ChannelItem from '@components/ChannelItem';
 import util from '@lib/util';
 import { HiOutlineSpeakerWave } from 'solid-icons/hi';
 import { OcHash3 } from 'solid-icons/oc';
-import { SelectedChannelIdContext } from '@lib/context/SelectedChannelId';
 import { ServerCollectionContext } from '@lib/context/collections/Servers';
 import { SelectedServerContext } from '@lib/context/SelectedServer';
 import { ChannelCollectionContext } from '@lib/context/collections/Channels';
+import { SelectedChannelIdContext } from '@lib/context/SelectedChannelId';
 
 export default function ServerWrapper() {
 	const selectedServer = useContext(SelectedServerIdContext);
-	const selectedChannelId = useContext(SelectedChannelIdContext);
-	const channelIsSelected = createSelector(selectedChannelId);
 
 	return (
 		<Show when={selectedServer() != undefined && selectedServer()}>
@@ -31,74 +21,10 @@ export default function ServerWrapper() {
 
 				return (
 					<Show when={server()} fallback={<p>Unresolved server</p>}>
-						{(serverAccesor) => {
-							function getChannel(id: string) {
-								const channel = channelCollection.get(id);
-								if (channel == undefined) {
-									return [];
-								}
-
-								return [channel[0]];
-							}
-
-							const server = () => serverAccesor()[0];
-							const channelCollection = useContext(ChannelCollectionContext);
-							const channels = createMemo(() => {
-								return {
-									unsorted: server().channels.flatMap((channel_id) => {
-										if (
-											server().categories != undefined &&
-											server().categories?.some((category) =>
-												category.channels.includes(channel_id)
-											)
-										) {
-											return [];
-										}
-
-										return getChannel(channel_id);
-									}),
-									categorized: server().categories?.flatMap((category) => {
-										if (category.channels.length == 0) {
-											return [];
-										}
-
-										return [
-											{
-												...category,
-												channels: category.channels.flatMap(getChannel)
-											}
-										];
-									})
-								};
-							});
-
+						{(server) => {
 							return (
-								<SelectedServerContext.Provider value={server}>
-									<div class="channel-bar-container">
-										<For each={channels().unsorted}>
-											{(channel) => (
-												<ChannelComponent
-													channel={channel}
-													selected={channelIsSelected(channel._id)}
-												/>
-											)}
-										</For>
-										<For each={channels().categorized}>
-											{(category) => (
-												<details open>
-													<summary>{category.title}</summary>
-													<For each={category.channels}>
-														{(channel) => (
-															<ChannelComponent
-																channel={channel}
-																selected={channelIsSelected(channel._id)}
-															/>
-														)}
-													</For>
-												</details>
-											)}
-										</For>
-									</div>
+								<SelectedServerContext.Provider value={() => server()[0]}>
+									<ChannelBar />
 									<Outlet />
 								</SelectedServerContext.Provider>
 							);
@@ -107,6 +33,77 @@ export default function ServerWrapper() {
 				);
 			}}
 		</Show>
+	);
+}
+
+function ChannelBar() {
+	const selectedChannelId = useContext(SelectedChannelIdContext);
+	const channelIsSelected = createSelector(selectedChannelId);
+
+	const server = useContext(SelectedServerContext);
+	const channelCollection = useContext(ChannelCollectionContext);
+
+	function getChannel(id: string) {
+		const channel = channelCollection.get(id);
+		if (channel == undefined) {
+			return [];
+		}
+
+		return [channel[0]];
+	}
+
+	const channels = createMemo(() => {
+		const s = server();
+		if (s == undefined) {
+			return { unsorted: [], categorized: [] };
+		}
+
+		return {
+			unsorted: s.channels.flatMap((channel_id) => {
+				if (
+					s.categories != undefined &&
+					s.categories?.some((category) => category.channels.includes(channel_id))
+				) {
+					return [];
+				}
+
+				return getChannel(channel_id);
+			}),
+			categorized: s.categories?.flatMap((category) => {
+				if (category.channels.length == 0) {
+					return [];
+				}
+
+				return [
+					{
+						...category,
+						channels: category.channels.flatMap(getChannel)
+					}
+				];
+			})
+		};
+	});
+
+	return (
+		<div class="channel-bar-container">
+			<For each={channels().unsorted}>
+				{(channel) => (
+					<ChannelComponent channel={channel} selected={channelIsSelected(channel._id)} />
+				)}
+			</For>
+			<For each={channels().categorized}>
+				{(category) => (
+					<details open>
+						<summary>{category.title}</summary>
+						<For each={category.channels}>
+							{(channel) => (
+								<ChannelComponent channel={channel} selected={channelIsSelected(channel._id)} />
+							)}
+						</For>
+					</details>
+				)}
+			</For>
+		</div>
 	);
 }
 
