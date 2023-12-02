da87ddbf96a1985cd5b0933b57e97037b36590a8
vloddot <t.mokhtar.vd@gmail.com>
2023-12-02 18:23:31 +0200

    feat: emojis (twemoji)

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -1,25 +1,69 @@
 import styles from './index.module.scss';
-import converter from '@lib/showdown';
 import DOMPurify from 'dompurify';
 import { createEffect, createMemo } from 'solid-js';
+import emojis from '@lib/emojis.json';
+import { RE_EMOJI } from '@lib/regex';
+import showdown from 'showdown';
 
 export interface Props {
 	content: string;
 }
 
-export default function Markdown(props: Props) {
-	const content = createMemo(() => {
-		const html = converter.makeHtml(
-			// https://github.com/markedjs/marked/issues/2139 (we don't use marked but this still applies)
-			// eslint-disable-next-line no-misleading-character-class
-			props.content.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/, '')
-		);
+const converter = new showdown.Converter({
+	ghCompatibleHeaderId: true,
+	openLinksInNewWindow: true,
+	strikethrough: true,
+	tables: true,
+	tasklists: true,
+	underline: true
+});
 
-		return DOMPurify.sanitize(html);
+export default function Markdown(props: Props) {
+	let ref: HTMLSpanElement;
+
+	createEffect(() => (ref.innerHTML = DOMPurify.sanitize(converter.makeHtml(props.content))));
+
+	createEffect(() => {
+		const emojiMatches = props.content.matchAll(RE_EMOJI);
+		for (const match of emojiMatches) {
+			const emoji = match[1];
+			if (emoji == undefined) {
+				continue;
+			}
+
+			let src: string | undefined;
+
+			const standard = (emojis.standard as Record<string, string>)[emoji];
+			if (standard == undefined) {
+				const custom = (emojis.custom as Record<string, string>)[emoji];
+				if (custom != undefined) {
+					src = `https://dl.insrt.uk/projects/revolt/emotes/${custom}`;
+				}
+			} else {
+				src = `https://static.revolt.chat/emoji/twemoji/${standard
+					.codePointAt(0) // emoji component
+					?.toString(16)}.svg`; // convert to hex
+			}
+
+			const img = document.createElement('img');
+			img.classList.add(styles.emoji);
+			img.src = src ?? `https://autumn.revolt.chat/emojis/${emoji}`;
+
+			img.onload = () => {
+				ref.innerHTML = ref.innerHTML.replace(new RegExp(`:${emoji}:`, 'g'), img.outerHTML);
+			};
+		}
 	});
 
-	createEffect(() => console.log(content()));
+	const isOnlyEmoji = createMemo(() => props.content.replace(RE_EMOJI, '').trim().length == 0);
 
-	// eslint-disable-next-line solid/no-innerhtml
-	return <span class={styles.markdownBase} innerHTML={content()} />;
+	return (
+		<span
+			style={{ '--emoji-size': isOnlyEmoji() ? '3em' : '1em' }}
+			class={styles.markdownBase}
+			ref={ref!}
+		>
+			{/*@once*/ props.content}
+		</span>
+	);
 }
