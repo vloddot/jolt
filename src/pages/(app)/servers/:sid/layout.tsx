21562e22cd8b80cfc39f445bcef948e2f0d06f53
vloddot <t.mokhtar.vd@gmail.com>
2023-12-07 10:13:53 +0200

    feat: server loading screen fix: hide categories without channels

--- src/pages/(app)/servers/:sid/layout.tsx
+++ src/pages/(app)/servers/:sid/layout.tsx
@@ -40,7 +40,7 @@ export default function ServerWrapper() {
 						{(server) => {
 							return (
 								<SelectedServerContext.Provider value={() => server()[0]}>
-									<Show when={members.state == 'ready'}>
+									<Show when={members.state == 'ready'} fallback={<p>Loading members...</p>}>
 										<ServerMembersListContext.Provider value={members}>
 											<ChannelBar />
 											<main class="main-content-container">
@@ -93,14 +93,15 @@ function ChannelBar() {
 				return getChannel(channel_id);
 			}),
 			categorized: s.categories?.flatMap((category) => {
-				if (category.channels.length == 0) {
+				const channels = category.channels.flatMap(getChannel);
+				if (channels.length == 0) {
 					return [];
 				}
 
 				return [
 					{
 						...category,
-						channels: category.channels.flatMap(getChannel)
+						channels
 					}
 				];
 			})
