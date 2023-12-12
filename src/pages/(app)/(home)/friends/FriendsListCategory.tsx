7d24bb829ffc3fb1eb4770efe79757b5f9a16361
vloddot <t.mokhtar.vd@gmail.com>
2023-12-12 12:01:28 +0200

    feat: outgoing friend request cancel and new icons

--- src/pages/(app)/(home)/friends/FriendsListCategory.tsx
+++ src/pages/(app)/(home)/friends/FriendsListCategory.tsx
@@ -2,9 +2,11 @@ import styles from './index.module.scss';
 import utilStyles from '@lib/util.module.scss';
 import UserButton from '@components/User/Button';
 import { For, Match, Show, Switch } from 'solid-js';
-import { CgUserAdd, CgUserRemove } from 'solid-icons/cg';
 import api from '@lib/api';
 import Tooltip from '@components/Tooltip';
+import { FaSolidUserMinus, FaSolidUserPlus, FaSolidUserSlash, FaSolidUserXmark } from 'solid-icons/fa';
+import type { IconTypes } from 'solid-icons';
+import { Dynamic } from 'solid-js/web';
 
 export interface Props {
 	title: string;
@@ -39,25 +41,48 @@ export default function FriendsListCategory(props: Props) {
 											style={{ '--hover': 'var(--success)' }}
 											onClick={() => api.acceptFriend(user._id)}
 										>
-											<CgUserAdd size="16" />
+											<FaSolidUserPlus size="16" />
 										</button>
 									</Tooltip>
 								</Show>
 								<Switch>
-									<Match when={user.relationship == 'Friend' || user.relationship == 'Incoming'}>
-										<Tooltip
-											content={
-												user.relationship == 'Friend' ? 'Remove Friend' : 'Deny Friend Request'
-											}
-										>
-											<button
-												class={styles.roundButton}
-												style={{ '--hover': 'var(--error)' }}
-												onClick={() => api.removeFriend(user._id)}
-											>
-												<CgUserRemove size="16" />
-											</button>
-										</Tooltip>
+									<Match
+										when={
+											user.relationship != undefined &&
+											(user.relationship == 'Friend' ||
+												user.relationship == 'Incoming' ||
+												user.relationship == 'Outgoing') &&
+											(user as User & { relationship: 'Friend' | 'Incoming' | 'Outgoing' })
+										}
+									>
+										{(user) => {
+											const tooltipContentMapping: Record<
+												'Friend' | 'Incoming' | 'Outgoing',
+												string
+											> = {
+												Friend: 'Remove Friend',
+												Incoming: 'Deny Friend Request',
+												Outgoing: 'Cancel Friend Request'
+											};
+
+											const iconMapping: Record<'Friend' | 'Incoming' | 'Outgoing', IconTypes> = {
+												Friend: FaSolidUserMinus,
+												Incoming: FaSolidUserSlash,
+												Outgoing: FaSolidUserXmark,
+											};
+
+											return (
+												<Tooltip content={tooltipContentMapping[user().relationship]}>
+													<button
+														class={styles.roundButton}
+														style={{ '--hover': 'var(--error)' }}
+														onClick={() => api.removeFriend(user()._id)}
+													>
+														<Dynamic component={iconMapping[user().relationship]} size="16" />
+													</button>
+												</Tooltip>
+											);
+										}}
 									</Match>
 								</Switch>
 							</div>
